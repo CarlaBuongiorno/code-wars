4883ea68848381443449752d21d3207b91e7f1b2
Carla Buongiorno <carla.buongiorno@fox-it.com>
2023-04-21 19:19:06 +0200

    Fix bug in conversion function

--- camel_case.py
+++ camel_case.py
@@ -12,17 +12,25 @@
 '''
 
 def main():
+    # convert_to_camel_casing(string_to_convert) # ['the', 'stealth', 'warrior']
     convert_to_camel_casing("the-stealth-warrior") # ['the', 'stealth', 'warrior']
     convert_to_camel_casing("The_Stealth_Warrior") # ['The', 'Stealth', 'Warrior']
     convert_to_camel_casing("The_Stealth-Warrior") # ['The', 'Stealth', 'Warrior']
+    convert_to_camel_casing("The_stealth-warrior") # ['The', 'stealth', 'warrior']
 
 def convert_to_camel_casing(string_to_convert):
     string_to_convert = string_to_convert.replace('-', ' ').replace('_', ' ').split()
-    if string_to_convert[0][0].islower():
-        conversion = [string_to_convert[i].capitalize() for i in range(1, len(string_to_convert))]
-        print(string_to_convert[0] + ''.join(conversion))
-    else:
-        print(''.join(string_to_convert))
+    first_letter = string_to_convert[0][0].lower()
+    conversion = [string_to_convert[i].capitalize() for i in range(1, len(string_to_convert))]
+    print(first_letter + ''.join(string_to_convert[0][1:]) + ''.join(conversion))
+
+
+# def check_is_valid(string_to_convert):
+#     if not isinstance(string_to_convert, str):
+#         raise TypeError('Invalid type for conversion')
+#     elif string_to_convert == '' or ' ' in string_to_convert:
+#         raise ValueError('Invalid value for conversion')
+
 
 if __name__ == '__main__':
     main()
